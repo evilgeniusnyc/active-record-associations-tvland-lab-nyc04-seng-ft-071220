3e3562f6a5c830bb49d3123a3af82d1a918784ba
fbado66 <fabdo66@gmail.com>
2020-08-13 17:40:22 -0400

    Building Associations on Models

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}" 
+    end 
+
+    def list_roles
+        # binding.pry
+        Character.all.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end 
+    end 
+
+
+
+        # expect(emilia.list_roles).to include("Khaleesi - Game of Thrones")
+
   
 end
+
+
+
